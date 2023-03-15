e13019d8a49e5f6c63234c539c235d06a08874ef
Uthaeus <romanlavery@gmail.com>
2023-03-15 14:32:09 -0600

    moved list item to GoalItem component

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
   Button,
-  ScrollView,
   FlatList,
 } from "react-native";
-import { TextInput } from "react-native-web";
+
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -37,11 +36,7 @@ export default function App() {
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={itemData => {
           
-          return (
-            <View style={styles.goalItem}>
-              <Text style={styles.goalText}>{itemData.item.text}</Text>
-            </View>
-          );
+          return <GoalItem />;
         }} 
           keyExtractor={(item, index) => {
             return item.id;
@@ -77,13 +72,5 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 5,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
-  },
+  
 });
